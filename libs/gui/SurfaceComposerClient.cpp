2aee796a6ed23ca9822e57412f46f4097ef528e4
Michael Limb <something15525@gmail.com>
2013-08-06 03:17:59 -0700

    frameworks/native: Fix toroplus radio with CFLAGS
    This is a commit to fix the LTE radio on toroplus,
    since the binaries contain a broken reference to
    an old no longer existent object. This fixes that
    with a CFLAGS to be enabled in the .mk files of the
    toroplus device files (like BoardConfig.mk).
    
    Credit for the original fix goes to
    Sean McClenaghan (cyberkitsune09@gmail.com)
    http://review.cyanogenmod.org/#/c/47134/
    
    PS 2-3: forgot some stuff in the commit message
    
    Change-Id: I2c0f05d1f70b96ad801971f23f6f9cf4e0fa7167

--- libs/gui/SurfaceComposerClient.cpp
+++ libs/gui/SurfaceComposerClient.cpp
@@ -646,14 +646,13 @@ void SurfaceComposerClient::unblankDisplay(const sp<IBinder>& token) {
     ComposerService::getComposerService()->unblank(token);
 }
 
-// TODO: Remove me.  Do not use.
-// This is a compatibility shim for one product whose drivers are depending on
-// this legacy function (when they shouldn't).
+#if defined(TOROPLUS_RADIO)
 status_t SurfaceComposerClient::getDisplayInfo(
         int32_t displayId, DisplayInfo* info)
 {
     return getDisplayInfo(getBuiltInDisplay(displayId), info);
 }
+#endif
 
 #if defined(ICS_CAMERA_BLOB) || defined(MR0_CAMERA_BLOB)
 ssize_t SurfaceComposerClient::getDisplayWidth(int32_t displayId) {
